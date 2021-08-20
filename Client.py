cb85fefa45aa0eebb06b739a562336653f961dfd
Mister Gardner <mbgardner02@gmail.com>
2021-08-20 13:14:21 -0500

    Corrected functionality for hashing info dict
    Changed from hashing decoded info dict to bencoded info dict in accordance with Bittorrent standard

--- Client.py
+++ Client.py
@@ -4,8 +4,8 @@ import hashlib
 
 file_in = sys.argv[1] # take torrent as input from command line
 torrent = open(f'C:/Users/Mister Gardner/Desktop/BitTorrent client project/{file_in}', 'rb')
-
-file_data = bencoder.decode(torrent.read()) #decode bencoded torrent file
+data = torrent.read()
+file_data = bencoder.decode(data) #decode bencoded torrent file
 new_dict = {}
 key_set=()
 wanted_keys = {b'piece length', b'pieces', b'name', b'length', b'files'} # required keys from metainfo
@@ -15,11 +15,12 @@ key_set = set(new_dict[b'info'].keys())
 unwanted = key_set - wanted_keys #filter for required dictionary keys
 for unwanted_key in unwanted: 
     del new_dict[b'info'][unwanted_key]
-    
-# hashing value of info key 
-hash_obj = hashlib.sha1()
-hash_data = str(new_dict[b'info'])
-hash_obj.update(hash_data.encode())
 
-print(hash_obj.digest())
+# hashing value of info key 
+bencoded_info = bencoder.encode(new_dict[b'info'])
+hash_obj = hashlib.sha1()
+hash_data = bencoded_info
+hash_obj.update(hash_data)
+
+print(hash_obj.hexdigest())
 
